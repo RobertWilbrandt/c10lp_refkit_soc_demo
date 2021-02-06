abbe073c27e485e41bf6f04bfb4c7e5496fe2d32
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-02-06 19:39:20 +0100

    Create package

---
+++ README.md
@@ -0,0 +1,4 @@
+c10lp_refkit_soc_demo
+=====================
+
+Small demo SoC and application for the Cyclone 10 LP RefKit, based on the [litex](https://github.com/enjoy-digital/litex) SoC builder.
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Demo SoC and application for the Cyclone 10 LP RefKit"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="c10lp_refkit_soc_demo",
+    version="0.1",
+    author="Robert Wilbrandt",
+    author_email="robert@stamm-wilbrandt.de",
+    description="Demo SoC and application for the Cyclone 10 LP RefKit",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    scripts=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
