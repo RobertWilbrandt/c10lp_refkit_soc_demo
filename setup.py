dc954ce0c0d64f4c1ace9b8a429c9f0b2743e4fc
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-02-06 19:45:05 +0100

    Add python setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    scripts=[],
+    scripts=["soc/c10lp-refkit-soc-demo"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
