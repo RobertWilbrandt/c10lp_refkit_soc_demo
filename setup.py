7c85316e4cf05f758ba474424eeba7f613d2eeba
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-02-09 18:01:36 +0100

    No longer install soc builder as script

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    scripts=["soc/c10lp-refkit-soc-demo"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
